c9807e22ab46b03a24b9dc8828253d326b807c2e
Ahmed Hussein <thelastrealdevil@gmail.com>
2015-11-03 13:07:21 +0200

    vs 2010 use boost and vs 2013 using std for chrono and threads

--- logger.hpp
+++ logger.hpp
@@ -5,10 +5,11 @@
 #include <cstring>    // memchr, strlen
 #include <sstream>    // std::stringstream
 #include <fstream>    // std::ostream
-#include <mutex>      // std::mutex, std::lock_guard
-#include <thread>     // std::thread::id
 #include <map>        // std::map
 
+#if(_MSC_VER == 1800)
+#include <mutex>      // std::mutex, std::lock_guard
+#include <thread>     // std::thread::id
 class LoggerSystem
 {
 public:
@@ -87,5 +88,87 @@ protected:
     _buffer.str(std::string());
   }
 };
+#else
+#include <boost/thread/mutex.hpp>      // std::mutex, std::lock_guard
+#include <boost/thread.hpp>            // std::thread::id
+class LoggerSystem
+{
+public:
+	enum Type{
+		Console = 0,
+		File,
+		GUI,
+		ConsoleAndFile
+	};
+protected:
+	std::map<boost::thread::id, std::stringstream> mBuffer;
+	Type mType;
+	std::ofstream mFile;
+	boost::mutex mMutex;
+public:
+	LoggerSystem(Type _type = Console)
+		: mType(_type){}
+	LoggerSystem(const std::string& _fileName, Type _type = File)
+		: mType(_type){
+			if(!_fileName.empty()){
+				mFile.open(_fileName);
+				if(!mFile.is_open()){
+					mType = Console;
+				}
+			}
+	}
+	~LoggerSystem(){
+		if(mFile.is_open()){
+			mFile.close();
+		}
+	}
+
+	LoggerSystem& operator <<(const std::string& _input){
+		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
+		boost::thread::id id = boost::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		if(_input.find('\n') != std::string::npos)
+			flush(buf);
+		return *this;
+	}
+
+	LoggerSystem& operator <<(const char* _input){
+		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
+		boost::thread::id id = boost::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		if(memchr(_input, '\n', strlen(_input)))
+			flush(buf);
+		return *this;
+	}
+
+	template<typename T>
+	LoggerSystem& operator <<(const T& _input){
+		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
+		boost::thread::id id = boost::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		return *this;
+	}
+protected:
+	void flush(std::stringstream& _buffer){
+		switch(mType){
+		case ConsoleAndFile:
+			mFile << _buffer.str();
+		case Console:
+			std::cout << _buffer.str();
+			break;
+		case File:
+			mFile << _buffer.str();
+			break;
+		case GUI:
+			break;
+		}
+		// Clear stringStream
+		_buffer.str(std::string());
+	}
+};
+#endif
 #endif // LOGGER
 
--- timer.hpp
+++ timer.hpp
@@ -1,22 +1,117 @@
 #ifndef TIMER_HPP
 #define TIMER_HPP
 #include <string>
-#include <chrono>
+#include <sstream>
 #include <iostream>
+#include "logger.hpp"
+
+#if(_MSC_VER == 1800)
+#include <chrono>
 class Timer{
   std::chrono::high_resolution_clock::time_point t1, t2;
+  LoggerSystem* mLogger;
 public:
-  Timer(){
-    t1 = std::chrono::high_resolution_clock::now();
-  }
-  void reset(){
-    t1 = std::chrono::high_resolution_clock::now();
-  }
-  void end(std::string log = "Timer Take"){
-    t2 = std::chrono::high_resolution_clock::now();
-    auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-    std::cout << log << " : " << total.count() << " milliseconds.\n";
-  }
+	Timer() : mLogger(nullptr){}
+	Timer(LoggerSystem* _logger) : mLogger(_logger){}
+	void start(){
+		t1 = std::chrono::high_resolution_clock::now();
+	}
+	void reset(){
+		t1 = std::chrono::high_resolution_clock::now();
+	}
+	void end(std::ostream _output, std::string log = "Timer Take"){
+		t2 = std::chrono::high_resolution_clock::now();
+		auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+		_output << log << " : " << total.count() << " milliseconds.\n";
+	}
+	void end(std::string log = "Timer Take"){
+		if(mLogger == nullptr)
+			return;
+		t2 = std::chrono::high_resolution_clock::now();
+		auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+		std::stringstream buffer;
+		buffer << log << " : " << total.count() << " milliseconds.\n";
+		(*mLogger) << buffer.str();
+	}
 };
+#else
+#include <boost\chrono.hpp>
+class Timer{
+	boost::chrono::high_resolution_clock::time_point t1, t2;
+	LoggerSystem* mLogger;
+public:
+	Timer(): mLogger(nullptr){}
+	Timer(LoggerSystem* _logger) : mLogger(_logger){
+	}
+	void start(){
+		t1 = boost::chrono::high_resolution_clock::now();
+	}
+	void reset(){
+		t1 = boost::chrono::high_resolution_clock::now();
+	}
+	void end(std::ostream _output, std::string log = "Timer Take"){
+		t2 = boost::chrono::high_resolution_clock::now();
+		auto total = boost::chrono::duration_cast<boost::chrono::milliseconds>(t2 - t1);
+		_output << log << " : " << total.count() << " milliseconds.\n";
+	}
+	void end(std::string log = "Timer Take"){
+		if(mLogger == nullptr)
+			return;
+		t2 = boost::chrono::high_resolution_clock::now();
+		auto total = boost::chrono::duration_cast<boost::chrono::milliseconds>(t2 - t1);
+		std::stringstream buffer;
+		buffer << log << " : " << total.count() << " milliseconds.\n";
+		(*mLogger) << buffer.str();
+	}
+};
+#endif
+
+#ifdef USE_CUDA
+#include <cuda_runtime_api.h>
+class CUDA_Timer{
+	cudaEvent_t t1, t2;
+	LoggerSystem* mLogger;
+public:
+	CUDA_Timer() : mLogger(nullptr){
+		cudaEventCreate(&t1);
+		cudaEventCreate(&t2);
+	}
+	CUDA_Timer(LoggerSystem* _logger) : mLogger(_logger){
+		cudaEventCreate(&t1);
+		cudaEventCreate(&t2);
+	}
+	~CUDA_Timer(){
+		cudaEventDestroy(t1);
+		cudaEventDestroy(t2);
+	}
+	void start(){
+		cudaEventRecord(t1);
+	}
+	void reset(){
+		cudaEventRecord(t1);
+	}
+	void end(std::ostream _output, std::string log = "Timer Take", cudaStream_t _stream = 0){
+		cudaEventRecord(t2);
+		cudaEventSynchronize(t2);
+		float time = 0.0f;
+		cudaEventElapsedTime(&time, t1, t2);
+
+		cudaEventCreate(&t1);
+		cudaEventRecord(t1, _stream);
+		_output << log << " : " << time << " milliseconds.\n";
+	}
+	void end(std::string log = "Timer Take"){
+		if (mLogger == nullptr)
+			return;
+		cudaEventRecord(t2);
+		cudaEventSynchronize(t2);
+		float time = 0.0f;
+		cudaEventElapsedTime(&time, t1, t2);
+		std::stringstream buffer;
+		buffer << log << " : " << time << " milliseconds.\n";
+		(*mLogger) << buffer.str();
+	}
+};
+#endif
 
 #endif//TIMER_HPP
