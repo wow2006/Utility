ba270e81f986ae008761d18aff890a90c362c0a2
Ahmed Hussein <thelastrealdevil@gmail.com>
2015-11-29 20:31:30 +0200

    Add CUDA and fix vs2010 support

--- logger.hpp
+++ logger.hpp
@@ -13,80 +13,80 @@
 class LoggerSystem
 {
 public:
-  enum class Type{
-    Console,
-    File,
-    GUI,
-    ConsoleAndFile
-  };
+	enum class Type{
+		Console,
+		File,
+		GUI,
+		ConsoleAndFile
+	};
 protected:
-  std::map<std::thread::id, std::stringstream> mBuffer;
-  Type mType = Type::Console;
-  std::ofstream mFile;
-  std::mutex mMutex;
+	std::map<std::thread::id, std::stringstream> mBuffer;
+	Type mType = Type::Console;
+	std::ofstream mFile;
+	std::mutex mMutex;
 public:
-  LoggerSystem(Type _type = Type::Console)
-    : mType(_type){}
-  LoggerSystem(const std::string& _fileName, Type _type = Type::File)
-    : mType(_type){
-    if(!_fileName.empty()){
-        mFile.open(_fileName);
-        if(!mFile.is_open()){
-            mType = Type::Console;
-        }
-    }
-  }
-  ~LoggerSystem(){
-    if(mFile.is_open()){
-        mFile.close();
-    }
-  }
+	LoggerSystem(Type _type = Type::Console)
+		: mType(_type){}
+	LoggerSystem(const std::string& _fileName, Type _type = Type::File)
+		: mType(_type){
+		if (!_fileName.empty()){
+			mFile.open(_fileName);
+			if (!mFile.is_open()){
+				mType = Type::Console;
+			}
+		}
+	}
+	~LoggerSystem(){
+		if (mFile.is_open()){
+			mFile.close();
+		}
+	}
 
-  LoggerSystem& operator <<(const std::string& _input){
-    std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-    std::thread::id id = std::this_thread::get_id();
-    std::stringstream& buf = mBuffer[id];
-    buf << _input;
-    if(_input.find('\n') != std::string::npos)
-      flush(buf);
-    return *this;
-  }
+	LoggerSystem& operator <<(const std::string& _input){
+		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
+		std::thread::id id = std::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		if (_input.find('\n') != std::string::npos)
+			flush(buf);
+		return *this;
+	}
 
-  LoggerSystem& operator <<(const char* _input){
-    std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-    std::thread::id id = std::this_thread::get_id();
-    std::stringstream& buf = mBuffer[id];
-    buf << _input;
-    if(memchr(_input, '\n', strlen(_input)))
-      flush(buf);
-    return *this;
-  }
+	LoggerSystem& operator <<(const char* _input){
+		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
+		std::thread::id id = std::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		if (memchr(_input, '\n', strlen(_input)))
+			flush(buf);
+		return *this;
+	}
 
-  template<typename T>
-  LoggerSystem& operator <<(const T& _input){
-    std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-    std::thread::id id = std::this_thread::get_id();
-    std::stringstream& buf = mBuffer[id];
-    buf << _input;
-    return *this;
-  }
+	template<typename T>
+	LoggerSystem& operator <<(const T& _input){
+		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
+		std::thread::id id = std::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+		buf << _input;
+		return *this;
+	}
 protected:
-  void flush(std::stringstream& _buffer){
-    switch(mType){
-      case Type::ConsoleAndFile:
-        mFile << _buffer.str();
-      case Type::Console:
-        std::cout << _buffer.str();
-        break;
-      case Type::File:
-        mFile << _buffer.str();
-        break;
-      case Type::GUI:
-        break;
-      }
-    // Clear stringStream
-    _buffer.str(std::string());
-  }
+	void flush(std::stringstream& _buffer){
+		switch (mType){
+		case Type::ConsoleAndFile:
+			mFile << _buffer.str();
+		case Type::Console:
+			std::cout << _buffer.str();
+			break;
+		case Type::File:
+			mFile << _buffer.str();
+			break;
+		case Type::GUI:
+			break;
+		}
+		// Clear stringStream
+		_buffer.str(std::string());
+	}
 };
 #else
 #include <boost/thread/mutex.hpp>      // std::mutex, std::lock_guard
@@ -110,15 +110,15 @@ public:
 		: mType(_type){}
 	LoggerSystem(const std::string& _fileName, Type _type = File)
 		: mType(_type){
-			if(!_fileName.empty()){
-				mFile.open(_fileName);
-				if(!mFile.is_open()){
-					mType = Console;
-				}
+		if (!_fileName.empty()){
+			mFile.open(_fileName);
+			if (!mFile.is_open()){
+				mType = Console;
 			}
+		}
 	}
 	~LoggerSystem(){
-		if(mFile.is_open()){
+		if (mFile.is_open()){
 			mFile.close();
 		}
 	}
@@ -128,7 +128,7 @@ public:
 		boost::thread::id id = boost::this_thread::get_id();
 		std::stringstream& buf = mBuffer[id];
 		buf << _input;
-		if(_input.find('\n') != std::string::npos)
+		if (_input.find('\n') != std::string::npos)
 			flush(buf);
 		return *this;
 	}
@@ -138,7 +138,7 @@ public:
 		boost::thread::id id = boost::this_thread::get_id();
 		std::stringstream& buf = mBuffer[id];
 		buf << _input;
-		if(memchr(_input, '\n', strlen(_input)))
+		if (memchr(_input, '\n', strlen(_input)))
 			flush(buf);
 		return *this;
 	}
@@ -151,9 +151,17 @@ public:
 		buf << _input;
 		return *this;
 	}
+	void flush(){
+		boost::thread::id id = boost::this_thread::get_id();
+		std::stringstream& buf = mBuffer[id];
+
+		mFile << buf.str();
+		mFile.flush();
+		buf.str(std::string());
+	}
 protected:
 	void flush(std::stringstream& _buffer){
-		switch(mType){
+		switch (mType){
 		case ConsoleAndFile:
 			mFile << _buffer.str();
 		case Console:
--- timer.hpp
+++ timer.hpp
@@ -8,8 +8,8 @@
 #if(_MSC_VER == 1800)
 #include <chrono>
 class Timer{
-  std::chrono::high_resolution_clock::time_point t1, t2;
-  LoggerSystem* mLogger;
+	std::chrono::high_resolution_clock::time_point t1, t2;
+	LoggerSystem* mLogger;
 public:
 	Timer() : mLogger(nullptr){}
 	Timer(LoggerSystem* _logger) : mLogger(_logger){}
@@ -25,7 +25,7 @@ public:
 		_output << log << " : " << total.count() << " milliseconds.\n";
 	}
 	void end(std::string log = "Timer Take"){
-		if(mLogger == nullptr)
+		if (mLogger == nullptr)
 			return;
 		t2 = std::chrono::high_resolution_clock::now();
 		auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
@@ -40,7 +40,7 @@ class Timer{
 	boost::chrono::high_resolution_clock::time_point t1, t2;
 	LoggerSystem* mLogger;
 public:
-	Timer(): mLogger(nullptr){}
+	Timer() : mLogger(nullptr){}
 	Timer(LoggerSystem* _logger) : mLogger(_logger){
 	}
 	void start(){
@@ -55,60 +55,12 @@ public:
 		_output << log << " : " << total.count() << " milliseconds.\n";
 	}
 	void end(std::string log = "Timer Take"){
-		if(mLogger == nullptr)
+		if (mLogger == nullptr)
 			return;
 		t2 = boost::chrono::high_resolution_clock::now();
 		auto total = boost::chrono::duration_cast<boost::chrono::milliseconds>(t2 - t1);
 		std::stringstream buffer;
 		buffer << log << " : " << total.count() << " milliseconds.\n";
-		(*mLogger) << buffer.str();
-	}
-};
-#endif
-
-#ifdef USE_CUDA
-#include <cuda_runtime_api.h>
-class CUDA_Timer{
-	cudaEvent_t t1, t2;
-	LoggerSystem* mLogger;
-public:
-	CUDA_Timer() : mLogger(nullptr){
-		cudaEventCreate(&t1);
-		cudaEventCreate(&t2);
-	}
-	CUDA_Timer(LoggerSystem* _logger) : mLogger(_logger){
-		cudaEventCreate(&t1);
-		cudaEventCreate(&t2);
-	}
-	~CUDA_Timer(){
-		cudaEventDestroy(t1);
-		cudaEventDestroy(t2);
-	}
-	void start(){
-		cudaEventRecord(t1);
-	}
-	void reset(){
-		cudaEventRecord(t1);
-	}
-	void end(std::ostream _output, std::string log = "Timer Take", cudaStream_t _stream = 0){
-		cudaEventRecord(t2);
-		cudaEventSynchronize(t2);
-		float time = 0.0f;
-		cudaEventElapsedTime(&time, t1, t2);
-
-		cudaEventCreate(&t1);
-		cudaEventRecord(t1, _stream);
-		_output << log << " : " << time << " milliseconds.\n";
-	}
-	void end(std::string log = "Timer Take"){
-		if (mLogger == nullptr)
-			return;
-		cudaEventRecord(t2);
-		cudaEventSynchronize(t2);
-		float time = 0.0f;
-		cudaEventElapsedTime(&time, t1, t2);
-		std::stringstream buffer;
-		buffer << log << " : " << time << " milliseconds.\n";
 		(*mLogger) << buffer.str();
 	}
 };
