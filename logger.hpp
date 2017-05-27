5cb52a199e6d3e53973b5947d01855fb18e3b9a7
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2017-05-27 23:00:17 +0200

    Enhance CMakeLists

--- logger.hpp
+++ logger.hpp
@@ -1,182 +1,105 @@
 #ifndef LOGGER
 #define LOGGER
-#include <iostream>   // std::cout
-#include <string>     // std::string
-#include <cstring>    // memchr, strlen
-#include <sstream>    // std::stringstream
-#include <fstream>    // std::ostream
-#include <map>        // std::map
+#include <iostream>  // std::cout
+#include <string>    // std::string
+#include <cstring>   // memchr, strlen
+#include <sstream>   // std::stringstream
+#include <fstream>   // std::ostream
+#include <map>       // std::map
 
-#if(_MSC_VER == 1800)
-#include <mutex>      // std::mutex, std::lock_guard
-#include <thread>     // std::thread::id
-class LoggerSystem
-{
-public:
-	enum class Type{
-		Console,
-		File,
-		GUI,
-		ConsoleAndFile
-	};
-protected:
-	std::map<std::thread::id, std::stringstream> mBuffer;
-	Type mType = Type::Console;
-	std::ofstream mFile;
-	std::mutex mMutex;
-public:
-	LoggerSystem(Type _type = Type::Console)
-		: mType(_type){}
-	LoggerSystem(const std::string& _fileName, Type _type = Type::File)
-		: mType(_type){
-		if (!_fileName.empty()){
-			mFile.open(_fileName);
-			if (!mFile.is_open()){
-				mType = Type::Console;
-			}
-		}
-	}
-	~LoggerSystem(){
-		if (mFile.is_open()){
-			mFile.close();
-		}
-	}
-
-	LoggerSystem& operator <<(const std::string& _input){
-		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-		std::thread::id id = std::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		if (_input.find('\n') != std::string::npos)
-			flush(buf);
-		return *this;
-	}
-
-	LoggerSystem& operator <<(const char* _input){
-		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-		std::thread::id id = std::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		if (memchr(_input, '\n', strlen(_input)))
-			flush(buf);
-		return *this;
-	}
-
-	template<typename T>
-	LoggerSystem& operator <<(const T& _input){
-		std::lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
-		std::thread::id id = std::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		return *this;
-	}
-protected:
-	void flush(std::stringstream& _buffer){
-		switch (mType){
-		case Type::ConsoleAndFile:
-			mFile << _buffer.str();
-		case Type::Console:
-			std::cout << _buffer.str();
-			break;
-		case Type::File:
-			mFile << _buffer.str();
-			break;
-		case Type::GUI:
-			break;
-		}
-		// Clear stringStream
-		_buffer.str(std::string());
-	}
-};
+#if !defined(USE_CXX98)
+#include <mutex>   // std::mutex, std::lock_guard
+#include <thread>  // std::thread::id
+using std::mutex;
+using std::thread;
+using std::lock_guard;
+using std::thread;
+using namespace std::this_thread;
+#elif defined(USE_BOOST)
+#include <boost/thread/mutex.hpp>  // std::mutex, std::lock_guard
+#include <boost/thread.hpp>        // std::thread::id
+using boost::mutex;
+using boost::thread;
+using boost::lock_guard;
+using boost::thread;
+using namespace boost::this_thread;
 #else
-#include <boost/thread/mutex.hpp>      // std::mutex, std::lock_guard
-#include <boost/thread.hpp>            // std::thread::id
-class LoggerSystem
-{
-public:
-	enum Type{
-		Console = 0,
-		File,
-		GUI,
-		ConsoleAndFile
-	};
-protected:
-	std::map<boost::thread::id, std::stringstream> mBuffer;
-	Type mType;
-	std::ofstream mFile;
-	boost::mutex mMutex;
-public:
-	LoggerSystem(Type _type = Console)
-		: mType(_type){}
-	LoggerSystem(const std::string& _fileName, Type _type = File)
-		: mType(_type){
-		if (!_fileName.empty()){
-			mFile.open(_fileName);
-			if (!mFile.is_open()){
-				mType = Console;
-			}
-		}
-	}
-	~LoggerSystem(){
-		if (mFile.is_open()){
-			mFile.close();
-		}
-	}
-
-	LoggerSystem& operator <<(const std::string& _input){
-		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
-		boost::thread::id id = boost::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		if (_input.find('\n') != std::string::npos)
-			flush(buf);
-		return *this;
-	}
-
-	LoggerSystem& operator <<(const char* _input){
-		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
-		boost::thread::id id = boost::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		if (memchr(_input, '\n', strlen(_input)))
-			flush(buf);
-		return *this;
-	}
-
-	template<typename T>
-	LoggerSystem& operator <<(const T& _input){
-		boost::lock_guard<boost::mutex> block_threads_until_finish_this_job(mMutex);
-		boost::thread::id id = boost::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-		buf << _input;
-		return *this;
-	}
-	void flush(){
-		boost::thread::id id = boost::this_thread::get_id();
-		std::stringstream& buf = mBuffer[id];
-
-		mFile << buf.str();
-		mFile.flush();
-		buf.str(std::string());
-	}
-protected:
-	void flush(std::stringstream& _buffer){
-		switch (mType){
-		case ConsoleAndFile:
-			mFile << _buffer.str();
-		case Console:
-			std::cout << _buffer.str();
-			break;
-		case File:
-			mFile << _buffer.str();
-			break;
-		case GUI:
-			break;
-		}
-		// Clear stringStream
-		_buffer.str(std::string());
-	}
-};
+#error("Can not use logger")
 #endif
-#endif // LOGGER
 
+class LoggerSystem {
+public:
+    enum class Type { Console, File, GUI, ConsoleAndFile };
+
+protected:
+    std::map<std::thread::id, std::stringstream> mBuffer;
+    Type mType = Type::Console;
+    std::ofstream mFile;
+    mutex mMutex;
+
+public:
+    LoggerSystem(Type _type = Type::Console) : mType(_type) {}
+
+    LoggerSystem(const std::string& _fileName, Type _type = Type::File)
+        : mType(_type) {
+        if (!_fileName.empty()) {
+            mFile.open(_fileName);
+            if (!mFile.is_open()) {
+                mType = Type::Console;
+            }
+        }
+    }
+
+    ~LoggerSystem() {
+        if (mFile.is_open()) {
+            mFile.close();
+        }
+    }
+
+    LoggerSystem& operator<<(const std::string& _input) {
+        lock_guard<std::mutex> block_threads_until_finish_this_job(mMutex);
+        thread::id id = get_id();
+        std::stringstream& buf = mBuffer[id];
+        buf << _input;
+        if (_input.find('\n') != std::string::npos) flush(buf);
+        return *this;
+    }
+
+    LoggerSystem& operator<<(const char* _input) {
+        lock_guard<mutex> block_threads_until_finish_this_job(mMutex);
+        thread::id id = get_id();
+        std::stringstream& buf = mBuffer[id];
+        buf << _input;
+        if (memchr(_input, '\n', strlen(_input))) flush(buf);
+        return *this;
+    }
+
+    template <typename T>
+    LoggerSystem& operator<<(const T& _input) {
+        lock_guard<mutex> block_threads_until_finish_this_job(mMutex);
+        thread::id id = get_id();
+        std::stringstream& buf = mBuffer[id];
+        buf << _input;
+        return *this;
+    }
+
+protected:
+    void flush(std::stringstream& _buffer) {
+        switch (mType) {
+            case Type::ConsoleAndFile:
+                mFile << _buffer.str();
+            case Type::Console:
+                std::cout << _buffer.str();
+                break;
+            case Type::File:
+                mFile << _buffer.str();
+                break;
+            case Type::GUI:
+                break;
+        }
+        // Clear stringStream
+        _buffer.str(std::string());
+    }
+};
+
+#endif  // LOGGER
--- timer.hpp
+++ timer.hpp
@@ -1,69 +1,45 @@
 #ifndef TIMER_HPP
 #define TIMER_HPP
-#include <string>
-#include <sstream>
-#include <iostream>
 #include "logger.hpp"
 
-#if(_MSC_VER == 1800)
+#if !defined(USE_CXX98)
 #include <chrono>
-class Timer{
-	std::chrono::high_resolution_clock::time_point t1, t2;
-	LoggerSystem* mLogger;
-public:
-	Timer() : mLogger(nullptr){}
-	Timer(LoggerSystem* _logger) : mLogger(_logger){}
-	void start(){
-		t1 = std::chrono::high_resolution_clock::now();
-	}
-	void reset(){
-		t1 = std::chrono::high_resolution_clock::now();
-	}
-	void end(std::ostream _output, std::string log = "Timer Take"){
-		t2 = std::chrono::high_resolution_clock::now();
-		auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-		_output << log << " : " << total.count() << " milliseconds.\n";
-	}
-	void end(std::string log = "Timer Take"){
-		if (mLogger == nullptr)
-			return;
-		t2 = std::chrono::high_resolution_clock::now();
-		auto total = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-		std::stringstream buffer;
-		buffer << log << " : " << total.count() << " milliseconds.\n";
-		(*mLogger) << buffer.str();
-	}
-};
-#else
+using namespace std::chrono;
+#elif defined(USE_BOOST)
 #include <boost\chrono.hpp>
-class Timer{
-	boost::chrono::high_resolution_clock::time_point t1, t2;
-	LoggerSystem* mLogger;
-public:
-	Timer() : mLogger(nullptr){}
-	Timer(LoggerSystem* _logger) : mLogger(_logger){
-	}
-	void start(){
-		t1 = boost::chrono::high_resolution_clock::now();
-	}
-	void reset(){
-		t1 = boost::chrono::high_resolution_clock::now();
-	}
-	void end(std::ostream _output, std::string log = "Timer Take"){
-		t2 = boost::chrono::high_resolution_clock::now();
-		auto total = boost::chrono::duration_cast<boost::chrono::milliseconds>(t2 - t1);
-		_output << log << " : " << total.count() << " milliseconds.\n";
-	}
-	void end(std::string log = "Timer Take"){
-		if (mLogger == nullptr)
-			return;
-		t2 = boost::chrono::high_resolution_clock::now();
-		auto total = boost::chrono::duration_cast<boost::chrono::milliseconds>(t2 - t1);
-		std::stringstream buffer;
-		buffer << log << " : " << total.count() << " milliseconds.\n";
-		(*mLogger) << buffer.str();
-	}
-};
+using namespace boost::chrono;
+#else
+#error("Can not use logger")
 #endif
 
-#endif//TIMER_HPP
+class Timer
+{
+    high_resolution_clock::time_point t1, t2;
+    LoggerSystem* mLogger;
+
+public:
+    Timer() : mLogger(nullptr) {}
+
+    Timer(LoggerSystem* _logger) : mLogger(_logger) {}
+
+    void start() { t1 = high_resolution_clock::now(); }
+
+    void reset() { t1 = high_resolution_clock::now(); }
+
+    void end(std::ostream _output, std::string log = "Timer Take") {
+        t2 = high_resolution_clock::now();
+        auto total = duration_cast<milliseconds>(t2 - t1);
+        _output << log << " : " << total.count() << " milliseconds.\n";
+    }
+
+    void end(std::string log = "Timer Take") {
+        if (mLogger == nullptr) return;
+        t2 = high_resolution_clock::now();
+        auto total = duration_cast<milliseconds>(t2 - t1);
+        std::stringstream buffer;
+        buffer << log << " : " << total.count() << " milliseconds.\n";
+        (*mLogger) << buffer.str();
+    }
+};
+
+#endif  // TIMER_HPP
